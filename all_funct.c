b97573bfdaf69b7ad1f20167d492da7d7363d834
nimrod02 <letrun.a@gmail.com>
2023-05-10 11:10:35 +0200

    add push function and update the monty.h

--- all_funct.c
+++ all_funct.c
@@ -1,42 +1,52 @@
 #include "monty.h"
 
 /**
- *push - 
+ * push - function that add an element to a stack
  * 
  * 
  * 
 */
 
-	void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *ptr = malloc(sizeof (stack_t));
+
+	int success = ptr != NULL;
+
+	if (success)
 	{
-
-
+		ptr->n = line_number;
+		ptr->next = *stack;
+		ptr->prev = NULL;
+		*stack = ptr;
 	}
 
-
-/**
- *pall - 
- * 
- * 
-*/
-
-	void pall(stack_t **stack, unsigned int line_number)
-	{
+	return (success);
+}
 
 
-	}
+// /**
+//  *pall - 
+//  * 
+//  * 
+// */
 
-/**
- *pint -  
- * 
- * 
- * 
-*/
-
-	void pint(stack_t **stack, unsigned int line_number)
-
-	{
+// void pall(stack_t **stack, unsigned int line_number)
+// {
 
 
-	}
+// }
+
+// /**
+//  *pint -  
+//  * 
+//  * 
+//  * 
+// */
+
+// void pint(stack_t **stack, unsigned int line_number)
+// {
+
+
+// }
 
--- monty.h
+++ monty.h
@@ -1,6 +1,12 @@
 #ifndef  __MONTY__
 #define  __MONTY__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
