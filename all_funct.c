222e1a9d5691dbf5bb78b47a93ba874c4b67ce8c
grem62 <gremontmatheo22@gmail.com>
2023-05-10 10:37:04 +0200

    add the opcode and the file with the function

---
+++ all_funct.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ *push - 
+ * 
+ * 
+ * 
+*/
+
+	void push(stack_t **stack, unsigned int line_number)
+	{
+
+
+	}
+
+
+/**
+ *pall - 
+ * 
+ * 
+*/
+
+	void pall(stack_t **stack, unsigned int line_number)
+	{
+
+
+	}
+
+/**
+ *pint -  
+ * 
+ * 
+ * 
+*/
+
+	void pint(stack_t **stack, unsigned int line_number)
+
+	{
+
+
+	}
+
--- monty.h
+++ monty.h
@@ -31,4 +31,12 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void opcode_funct(char *opcode, stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+
+
+
+
 #endif
