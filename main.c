392186cb3973ac88c4b58b683fd8549490d38918
grem62 <gremontmatheo22@gmail.com>
2023-05-13 19:44:42 +0200

    add hold file main

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
  *main - function use all function
  *@argc: counter of argument
  *@argv: value of argument
- *freeList - function free the stack
- *Return: return succes or fail
+ *freeList - function free the stack 
+ *return: return succes or fail
  *@h: pointer to pointer of struct stack_t
 */
 
 int main(int argc, char *argv[])
 {
+
 	stack_t *h = NULL;
 	int fileOpen, fileContent, lineCounter;
 	bool isPush = false;
@@ -21,35 +22,70 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-		fileOpen = open(argv[1], O_RDONLY);
 
-		if (fileOpen == -1)
+	fileOpen = open(argv[1], O_RDONLY);
+
+	if (fileOpen == -1)
 	{
-		fprintf(stderr, "Error: Can't open file %s", argv[1]);
+		fprintf(stderr, "Error: can't open file %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-		buffer = malloc(sizeof(char) * 17000);
-		if (buffer == NULL)
+
+	buffer = malloc(sizeof(char) * 17000);
+	if (buffer == NULL)
 	{
+
 		fprintf(stderr, "Error: malloc failed");
 		free(buffer);
 		exit(EXIT_FAILURE);
 	}
-		fileContent = read(fileOpen, buffer, 10000);
 
-		if (fileContent == -1)
+	fileContent = read (fileOpen, buffer, 10000);
+
+	if (fileContent == -1)
 	{
-		free(buffer);
-		close(fileOpen);
-		exit(EXIT_FAILURE);
+		free (buffer);
+		close (fileOpen);
+		exit (EXIT_FAILURE);
 	}
-		token = strtok(buffer, DELIM);
-		lineCounter = 1;
+	
+	token = strtok(buffer, DELIM);
 
-	TOKEN;
-	freeList(&h);
-	free(buffer);
+	lineCounter = 1;
+	while (token != NULL)
+	{
+		if (isPush == 1)
+		{
+			push(&h, lineCounter, token);
+			isPush = false;
+			token = strtok(NULL, DELIM);
+			lineCounter++;
+			continue;
+		}
+		else if (strcmp(token, "push") == 0)
+		{
+			isPush = true;
+			token = strtok(NULL, DELIM);
+			continue;
+		}
+		else
+		{
+			if (opcodeFunc(token) != NULL)
+				opcodeFunc(token)(&h, lineCounter);
+			else
+			{
+				freeList (&h);
+				fprintf(stderr, "L%d: unknown instruction %s\n", lineCounter, token);
+				exit (EXIT_FAILURE);
+			}
+		}
+		lineCounter++;
+		token = strtok(NULL, DELIM);
+	}
+
+	freeList (&h);
+	free (buffer);
 	close(fileOpen);
+
 	return (EXIT_SUCCESS);
 }
-
