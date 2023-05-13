0a72d158e33e895a49532cd16d9769a1a955b3dd
grem62 <gremontmatheo22@gmail.com>
2023-05-13 13:54:44 +0200

    add nop

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ *nop - function print nothing
+ *@stack:  double pointeur to the head ofstack
+ *@line_number: line number of read line
+*/
+
+		void nop(stack_t **stack, unsigned int line_number)
+{
+		(void) *stack;
+		(void) line_number;
+}
+
