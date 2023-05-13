93ee35aafa84af88f4b64391ef777eaddad5de57
Nimrod02 <86027619+Nimrod02@users.noreply.github.com>
2023-05-13 22:12:36 +0200

    Update add.c

--- add.c
+++ add.c
@@ -5,24 +5,21 @@
  * @stack: top of the list
  * @lineNumber: line number
 */
+
 void add(stack_t **stack, unsigned int lineNumber)
 {
-        /* Vérifie si la pile est vide ou a un seul élément */
         if (*stack == NULL || (*stack)->next == NULL)
         {
                 fprintf(stderr, "L%u: can't add, stack too short\n", lineNumber);
                 exit(EXIT_FAILURE);
         }
 
-        /* Ajoute la valeur du premier élément à la valeur du deuxième élément */
         (*stack)->next->n += (*stack)->n;
 
-        /* Déplace le pointeur du sommet de la pile vers le deuxième élément */
         (*stack) = (*stack)->next;
 
-        /* Libère la mémoire de l'ancien premier élément */
         free((*stack)->prev);
 
-        /* Met à jour le pointeur vers le précédent du nouveau premier élément */
         (*stack)->prev = NULL;
 }
+
