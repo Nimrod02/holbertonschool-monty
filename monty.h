3ff2113e369551ccc627fcc7090e56c4c15276e0
Nimrod02 <86027619+Nimrod02@users.noreply.github.com>
2023-05-13 22:18:39 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -41,7 +41,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number, char *token)
+void push(stack_t **stack, unsigned int line_number, char *token);
 void (*opcodeFunc(char *opCode))(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
